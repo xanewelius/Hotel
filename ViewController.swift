2a8c5b619178825d304359c0018c599c1e6646ef
Max <sleynip@inbox.ru>
2023-09-11 16:31:29 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Hotel
+//
+//  Created by Max Kuzmin on 11.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
